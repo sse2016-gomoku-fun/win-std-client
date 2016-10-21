eba590b4774723b1611df3aebee05cf9c69aefbe
Sweet <505968815@qq.com>
2016-10-21 00:30:20 -0500

    添加注释

--- src/main.c
+++ src/main.c
@@ -29,28 +29,47 @@ char board[BOARD_SIZE][BOARD_SIZE] = {0};
 
 void debug(const char *str)
 {
-	printf("DEBUG %s\n", str);
-	fflush(stdout);
+    printf("DEBUG %s\n", str);
+    fflush(stdout);
 }
 
 /*
  * YOUR CODE BEGIN
+ * 你的代码开始
  */
  
 /* 
  * You can define your own struct and variable here
+ * 你可以在这里定义你自己的结构体和变量
  */
- 
-void start()
+
+
+/*
+ * You should init your ai here
+ * 在这里初始化你的ai
+ */
+void initAI()
 {
-    memset(board, 0, sizeof(board));
+
 }
 
-struct Position begin(const char board[BOARD_SIZE][BOARD_SIZE], int me)
+/*
+ * Game Start, you will put the first chess.
+ * Warning: This method will only be called when after the initialize ofi the  map, it is your turn to put the chess. 
+ * Or this method will not be called.
+ * You should return a valid Position variable.
+ * 棋局开始，第一个由你来落子
+ * 请注意：只有在当棋局初始化后，轮到你落子时才会触发这个函数。如果在棋局初始化后，轮到对方落子时，这个函数不会被触发。详见项目要求。
+ * me的取值只可能是ME(1)，即board(棋盘)上为ME(1)的位置表示你的棋子，board(棋盘)上为OTHER(2)的位置表示对手的棋子。
+ * 返回一个结构体Position，你需要填充其中的x属性和y属性。
+ */
+struct Position aiBegin(const char board[BOARD_SIZE][BOARD_SIZE], int me)
 {
     /*
      * TODO: Write your own ai here!
-     * Here is a simple ai which just put chess at empty position!
+     * Here is a simple AI which just put chess at empty position!
+     * 待做: 在这里写下你的AI
+     * 这里有一个只会寻找第一个可下位置的人工智障！
      */
     int i, j;
     struct Position preferedPos;
@@ -66,15 +85,24 @@ struct Position begin(const char board[BOARD_SIZE][BOARD_SIZE], int me)
             }
         }
     
-    
     return preferedPos;
 }
 
-struct Position ai(const char board[BOARD_SIZE][BOARD_SIZE], int me, int otherX, int otherY)
+
+/*
+ * Game ongoing, the competitor put the chess at the position (otherX, otherY). You should put your chess.
+ * You should return a valid Position variable.
+ * 棋局进行中，对方落子在(otherX, otherY)的位置，轮到你落子了。
+ * me的取值只可能是ME(1)，即board(棋盘)上为ME(1)的位置表示你的棋子，board(棋盘)上为OTHER(2)的位置表示对手的棋子。
+ * 返回一个结构体Position，你需要填充其中的x属性和y属性。
+ */
+struct Position aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int me, int otherX, int otherY)
 {
     /*
      * TODO: Write your own ai here!
-     * Here is a simple ai which just put chess at empty position!
+     * Here is a simple AI which just put chess at empty position!
+     * 待做: 在这里写下你的AI
+     * 这里有一个只会寻找第一个可下位置的人工智障！
      */
     int i, j;
     struct Position preferedPos;
@@ -96,6 +124,7 @@ struct Position ai(const char board[BOARD_SIZE][BOARD_SIZE], int me, int otherX,
 
 /*
  * YOUR CODE END
+ * 你的代码结束
  */
 
 void place(int x, int y, int z)
@@ -109,12 +138,18 @@ void done()
     fflush(stdout);
 }
 
+void start()
+{
+    memset(board, 0, sizeof(board));
+    initAI();
+}
+
 void turn(int x, int y)
 {
     board[x][y] = OTHER;
     
     // AI
-    struct Position pos = ai(board, ME, x, y);
+    struct Position pos = aiTurn(board, ME, x, y);
     printf("%d %d\n", pos.x, pos.y);
     fflush(stdout);
 }
@@ -149,7 +184,7 @@ void loop()
         else if (strstr(buffer, BEGIN))
         {
     		// AI
-    		struct Position pos = begin(board, ME);
+    		struct Position pos = aiBegin(board, ME);
     		printf("%d %d\n", pos.x, pos.y);
     		fflush(stdout);
 		}
